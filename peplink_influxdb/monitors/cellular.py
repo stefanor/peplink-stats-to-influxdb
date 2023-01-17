7a419686981898275d598a32651b1b397d107d34
Stefano Rivera <stefano@rivera.za.net>
2023-01-17 18:01:30 -0400

    A numeric cellular generation

--- peplink_influxdb/monitors/cellular.py
+++ peplink_influxdb/monitors/cellular.py
@@ -1,9 +1,12 @@
 from typing import Generator
+import logging
 import re
 
 from peplink_api.services import PepLinkClientService
 
 from .base import Measurement, Monitor
+
+log = logging.getLogger(__name__)
 
 
 class CellularMonitor(Monitor):
@@ -15,15 +18,31 @@ class CellularMonitor(Monitor):
             data = status[str(id_)]
             if data["type"] != "cellular":
                 continue
+
+            dataTech = data["cellular"]["dataTechnology"]
             network = Measurement(
                 "cellular.network",
                 {"wan": id_},
                 {
-                    "technology": data["cellular"]["dataTechnology"],
+                    "technology": dataTech,
                     "mcc": int(data["cellular"]["mcc"]),
                     "mnc": int(data["cellular"]["mnc"]),
                 },
             )
+            # LTE is the only value seen so far in the wild
+            generation = {
+                "5G": 5.0,
+                "LTE": 4.0,
+                "HSPA": 3.5,
+                "UMTS": 3.0,
+                "EGPRS": 2.5,
+                "GPRS": 2.0,
+            }.get(dataTech)
+            if generation is None:
+                log.error("Unknown dataTechnology: %s", dataTech)
+            else:
+                network.fields["generation"] = generation
+
             signal = Measurement(
                 "cellular.signal",
                 {"wan": id_},
